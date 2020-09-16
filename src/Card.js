fd72f83b3c8bb4d5218b8dcb3da49b68ea950061
SashaRadovic <sasa.a.radovic@gmail.com>
2020-09-16 17:09:16 +0200

    Update Card.js

--- src/Card.js
+++ src/Card.js
@@ -1,18 +1,22 @@
 import React from "react";
 import "./styles.css";
 
-export default function Card(props) {
-  return (
-    <div className="App">
-      <p className="topLeft">
-        {props.name}
-        {props.sign}
-      </p>
 
-      <p className="bottomRight">
-        {props.name}
-        {props.sign}
-      </p>
-    </div>
+export default function Card(props) {
+
+
+
+
+
+  
+  const source =`url('${props.src}')`
+  const imgSrc={
+    backgroundImage:  source,
+  }
+  console.log(typeof(props.src))
+  return (
+    <div className="App"  style={imgSrc}>
+      
+ </div>
   );
 }
